240c6bc000993c17639fe834b87e31942fc3dd52
Sebastián Varela <sebastian.varela@s3ba.net>
2017-01-17 21:01:04 +0100

    Add empty project

---
+++ AlertController/AlertController.h
@@ -0,0 +1,19 @@
+//
+//  AlertController.h
+//  AlertController
+//
+//  Created by Sebastián Varela on 17/1/17.
+//  Copyright © 2017 sebastianvarela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlertController.
+FOUNDATION_EXPORT double AlertControllerVersionNumber;
+
+//! Project version string for AlertController.
+FOUNDATION_EXPORT const unsigned char AlertControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlertController/PublicHeader.h>
+
+
